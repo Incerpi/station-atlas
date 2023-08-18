34d593cffef8b08e91a957a1b66f5fc5260b7d8c
logonrmlocal <logonrmlocal@PA710MICRO21>
2023-08-18 10:39:09 -0300

    Add project files.

---
+++ station-atlas/Models/Usuario.cs
@@ -0,0 +1,15 @@
+﻿namespace station_atlas.Models
+{
+    public class Usuario
+    {
+        public int UsuarioId { get; set; }
+
+        public string Nome { get; set; }
+
+        public string Email { get; set;}
+
+        public string Cpf { get; set;}
+
+        public string Senha { get; set;}
+    }
+}
